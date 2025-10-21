cf1a1b78551b405bda53746349eff7efba671273
JavaSwing.test <jennifer020605@edu.unipar.br>
2025-10-21 20:46:49 -0300

    finalizando

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,7 @@ function App() {
 
       <div className="form">
         <div className="linha">
+        <label>Destino</label>
         <input
           type="text"
           placeholder="Digite aqui"
@@ -74,21 +75,81 @@ function App() {
           onChange={(e) => setDestino(e.target.value)}
         />
 
-        <button>Adicionar</button>
+        <label>Data</label>
+        <input
+          type="date"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+          />
+
+        </div>
+
+        <div className="linha">
+          <label>Valor Gasto (R$)</label>
+          <input
+            type="number"
+            value={gasto}
+            onChange={(e) => setGasto(e.target.value)}
+            />
+
+          <label>KM Rodados</label>
+          <input
+            type="number"
+            value={km}
+            onChange={(e) => setReembolso (e.target.value)}
+          />
+
+        <button onClick={addViagem} className='btn-add'>
+          + Adicionar Viagem ✔
+        </button>
         </div>
       </div>
 
-      <ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Data</th>
+            <th>Destino</th>
+            <th>Gasto</th>
+            <th>KM</th>
+            <th>Reembolso</th>
+            <th>Saldo</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
 
-        <li>
-          Tarefa
-          <div>
-            <button className="complete-btn" >✔</button>
-            <button className="delete-btn" >❌</button>
+        <tbody>
+          {viagens.map((v) => (
+            <tr key={v.id}>
+              <td>{new Date(v.data).toLocaleDateString()}</td>
+              <td>{v.destino}</td>
+              <td>{v.gasto.toFixed(2)}</td>
+              <td>{v.km}</td>
+              <td>{v.reembolso.toFixed(2)}</td>
+              <td style={{ color : v.saldo >= 0 ? "green" : "red"}}>
+                {v.saldo >= 0 ? "+" : ""}
+                {v.saldo.toFixed(2)}
+              </td>
+              <td>
+                <button onClick={() => excluirViagem(v.id)} classname="btn-del">X</button>
+              </td>
+            </tr>
+        ))}
+        </tbody>
+      </table>
+
+      {viagens.lenght > 0 && (
+        <div className="resumo">
+          <p>total gasto: R$ {totalGasto.toFixed(2)}</p>
+          <p>Total KM: {totalKm} Km</p>
+          <p>Total Reembolso: R$ {totalReembolso.toFixed(2)}</p>
+          <p style={{ color: saldoFinal >= 0 ? "green" : "red"}}>
+            Saldo Final: {saldoFinal >= 0 ? "+" : ""}
+            {saldoFinal.toFixed(2)}
+          </p>
+          <p>Média de KM/Viagem : {MediaKeyMessageEvent.toFixed(0)} km</p>
           </div>
-        </li>
-
-      </ul>
+      )}
     </div>
   );
 }
